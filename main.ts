9559faf152f8f15c0ba49f8a5773c82ecc25a485
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-03-04 13:34:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,33 +2,42 @@
 input.onButtonPressed(Button.A, function () {
     _1 += 1
     basic.showNumber(_1)
-    basic.pause(1000)
+    basic.pause(500)
     basic.clearScreen()
 })
 // Shows both numbers
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(_1)
-    basic.pause(200)
-    basic.showNumber(_2)
-    basic.pause(1000)
-    basic.clearScreen()
+    if (_3 > 0 && _4 > 0) {
+        basic.showNumber(_2)
+        basic.pause(200)
+        basic.showNumber(_1)
+        basic.clearScreen()
+    } else {
+        basic.showNumber(_1)
+        basic.pause(200)
+        basic.showNumber(_2)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
 })
 // Increases the second number by 1
 input.onButtonPressed(Button.B, function () {
     _2 += 1
     basic.showNumber(_2)
-    basic.pause(1000)
+    basic.pause(500)
     basic.clearScreen()
 })
 // Shows both numbers in reverse order
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(_2)
-    basic.pause(200)
-    basic.showNumber(_1)
-    basic.clearScreen()
+    _3 = _1
+    _4 = _2
 })
 // Sets both numbers to 0
 let _2 = 0
 let _1 = 0
+let _4 = 0
+let _3 = 0
 _1 = 0
 _2 = 0
+_3 = 0
+_4 = 0
